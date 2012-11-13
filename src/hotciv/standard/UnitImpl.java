64c5492ff61255b041f0191078b85c108ce47f2c
Jakob Ørhøj <jakobii@msn.com>
2012-11-13 23:49:32 +0100

    AlphaCiv 2.0
    Problemer med GitHub så beskeden forsvandt.
    
    Tilføjet tests på units, movement der er tilladt, attacking moves også.

---
+++ src/hotciv/standard/UnitImpl.java
@@ -0,0 +1,11 @@
+package hotciv.standard;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Orhoj
+ * Date: 13-11-12
+ * Time: 20:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class UnitImpl {
+}
