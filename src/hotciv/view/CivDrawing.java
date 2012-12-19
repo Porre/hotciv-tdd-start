76e75b2ef6d140bb90f3a7a8d4e5b15b60c95fd0
Jakob K. Ørhøj <jakobii@msn.com>
2012-12-19 12:03:45 +0100

    copyResource

--- src/hotciv/view/CivDrawing.java
+++ src/hotciv/view/CivDrawing.java
@@ -9,135 +9,125 @@ import minidraw.framework.*;
 import minidraw.standard.*;
 import minidraw.standard.handlers.*;
 
-/** CivDrawing is a specialized Drawing (model component) from
- * MiniDraw that dynamically builds the list of Figures for MiniDraw
- * to render the Unit and other information objects that are visible
- * in the Game instance.
- *
- * This is a TEMPLATE for the dSoftArk Exercise! This means
- * that it is INCOMPLETE and that there are several options
- * for CLEANING UP THE CODE when you add features to it!
+public class CivDrawing extends StandardDrawing implements Drawing, GameObserver {
 
-   This source code is from the book 
-     "Flexible, Reliable Software:
-       Using Patterns and Agile Development"
-     published 2010 by CRC Press.
-   Author: 
-     Henrik B Christensen 
-     Computer Science Department
-     Aarhus University
-   
-   This source code is provided WITHOUT ANY WARRANTY either 
-   expressed or implied. You may study, use, modify, and 
-   distribute it for non-commercial purposes. For any 
-   commercial use, see http://www.baerbak.com/
- 
-*/
+    protected Game game;
+    protected Map<Unit, UnitFigure> figureMap = null;
+    protected Map<Unit, Figure> cityMap = null;
+    private ImageFigure turnShieldIcon, unitShieldIcon, cityShieldIcon,
+            workforceFocusIcon, productionIcon;
+    private TextFigure moveCounter, gameAge;
 
-public class CivDrawing extends StandardDrawing
-  implements Drawing, GameObserver {
+    public CivDrawing(DrawingEditor editor, Game game) {
+        super();
+        this.game = game;
+        game.addObserver(this);
+        defineUnitMap();
+        defineIcons();
+    }
 
-  /** the Game instance that this UnitDrawing is going to render units
-   * from */
-  protected Game game;
-  
-  public CivDrawing( DrawingEditor editor, Game game ) {
-    super();
-    this.game = game;
+    private void defineIcons() {
+        Point turnShieldPoint = new Point(GfxConstants.TURN_SHIELD_X, GfxConstants.TURN_SHIELD_Y);
+        Point unitShieldPoint = new Point(GfxConstants.UNIT_SHIELD_X, GfxConstants.UNIT_SHIELD_Y);
+        Point cityShieldPoint = new Point(GfxConstants.CITY_SHIELD_X, GfxConstants.CITY_SHIELD_Y);
+        Point workforceFocusPoint = new Point(GfxConstants.WORKFORCEFOCUS_X, GfxConstants.WORKFORCEFOCUS_Y);
+        Point productionPoint = new Point(GfxConstants.CITY_PRODUCTION_X, GfxConstants.CITY_PRODUCTION_Y);
 
-    // register this unit drawing as listener to any game state
-    // changes...
-    game.addObserver(this);
-    // ... and build up the set of figures associated with
-    // units in the game.
-    defineUnitMap();
-    // and the set of 'icons' in the status panel
-    defineIcons();
-  }
-  
-  /** The UnitDrawing should not allow client side
-   * units to add and manipulate figures; only figures
-   * that renders game objects are relevant, and these
-   * should be handled by observer events from the game
-   * instance. Thus this method is 'killed'.
-   */
-  public Figure add(Figure arg0) {
-    throw new RuntimeException("Should not be used...");
-  }
+        turnShieldIcon = new ImageFigure("redshield", turnShieldPoint);
+        unitShieldIcon = new ImageFigure(GfxConstants.NOTHING, unitShieldPoint);
+        cityShieldIcon = new ImageFigure(GfxConstants.NOTHING, cityShieldPoint);
+        workforceFocusIcon = new ImageFigure(GfxConstants.NOTHING, workforceFocusPoint);
+        productionIcon = new ImageFigure(GfxConstants.NOTHING, productionPoint);
 
-  /** store all moveable figures visible in this drawing = units */
-  protected Map<Unit,UnitFigure> figureMap = null;
-   
-  /** erase the old list of units, and build a completely new
-   * one from scratch by iterating over the game world and add
-   * Figure instances for each unit in the world.
-   */
-  private void defineUnitMap() {
-    // ensure no units of the old list are accidental in
-    // the selection!
-    clearSelection();
+        moveCounter = new TextFigure("", new Point(GfxConstants.UNIT_COUNT_X, GfxConstants.UNIT_COUNT_Y));
+        gameAge = new TextFigure("", new Point(GfxConstants.AGE_TEXT_X, GfxConstants.AGE_TEXT_Y));
+        setGameAge(game.getAge());
 
-    figureMap = new HashMap<Unit,UnitFigure>();
-    Position p;
-    for ( int r = 0; r < GameConstants.WORLDSIZE; r++ ) {
-      for ( int c = 0; c < GameConstants.WORLDSIZE; c++ ) {
-        p = new Position(r,c);
-        Unit unit = game.getUnitAt(p);
-        if ( unit != null ) {
-          String type = unit.getTypeString();
-          // convert the unit's Position to (x,y) coordinates
-          Point point = new Point( GfxConstants.getXFromColumn(p.getColumn()),
-                                   GfxConstants.getYFromRow(p.getRow()) );
-          UnitFigure unitFigure =
-            new UnitFigure( type, point, unit );
-          unitFigure.addFigureChangeListener(this);
-          figureMap.put(unit, unitFigure);
+        super.add(turnShieldIcon);
+        super.add(unitShieldIcon);
+        super.add(cityShieldIcon);
+        super.add(workforceFocusIcon);
+        super.add(productionIcon);
+        super.add(moveCounter);
+        super.add(gameAge);
+    }
 
-          // also insert in superclass list as it is
-          // this list that is iterated by the
-          // graphics rendering algorithms
-          super.add(unitFigure);
+
+    public Figure add(Figure arg0) {
+        throw new RuntimeException("Should not be used...");
+    }
+
+    private void defineUnitMap() {
+        clearSelection();
+
+        figureMap = new HashMap<Unit, UnitFigure>();
+        Position p;
+        for (int r = 0; r < GameConstants.WORLDSIZE; r++) {
+            for (int c = 0; c < GameConstants.WORLDSIZE; c++) {
+                p = new Position(r, c);
+                Unit unit = game.getUnitAt(p);
+                if (unit != null) {
+                    String type = unit.getTypeString();
+                    Point point = new Point(GfxConstants.getXFromColumn(p.getColumn()),
+                            GfxConstants.getYFromRow(p.getRow()));
+                    UnitFigure unitFigure =
+                            new UnitFigure(type, point, unit);
+                    unitFigure.addFigureChangeListener(this);
+                    figureMap.put(unit, unitFigure);
+                    super.add(unitFigure);
+                }
+            }
         }
-      }
     }
-  }
 
-  private ImageFigure turnShieldIcon;
-  private void defineIcons() {
-    // very much a template implementation :)
-    turnShieldIcon = 
-      new ImageFigure( "redshield",
-                       new Point( GfxConstants.TURN_SHIELD_X,
-                                  GfxConstants.TURN_SHIELD_Y ) ); 
-    // insert in superclass figure list to ensure graphical
-    // rendering.
-    super.add(turnShieldIcon);
-  }
- 
-  // === Observer Methods ===
 
-  public void worldChangedAt(Position pos) {
-    System.out.println( "UnitDrawing: world changes at "+pos);
-    clearSelection();
-    // this is a really brute-force algorithm: destroy
-    // all known units and build up the entire set again
-    for ( Figure f : figureMap.values() ) {
-      super.remove(f);
+
+    // === Observer Methods ===
+
+    public void worldChangedAt(Position pos) {
+        System.out.println("UnitDrawing: world changes at " + pos);
+        clearSelection();
+        // this is a really brute-force algorithm: destroy
+        // all known units and build up the entire set again
+        for (Figure f : figureMap.values()) {
+            super.remove(f);
+        }
+
+        defineUnitMap();
     }
-    defineUnitMap();
-  }
 
-  public void turnEnds(Player nextPlayer, int age) {
-    System.out.println( "UnitDrawing: turnEnds for "+
-                        nextPlayer+" at "+age );
-    String playername = "red";
-    if ( nextPlayer == Player.BLUE ) { playername = "blue"; }
-    turnShieldIcon.set( playername+"shield",
-                        new Point( GfxConstants.TURN_SHIELD_X,
-                                   GfxConstants.TURN_SHIELD_Y ) );
-  }
+    public void turnEnds(Player nextPlayer, int age) {
+        // System.out.println("UnitDrawing: turnEnds for " + nextPlayer + " at " + age);
+        String playername = "red";
+        if (nextPlayer == Player.BLUE) {
+            playername = "blue";
+        }
+        turnShieldIcon.set(playername + "shield", new Point(GfxConstants.TURN_SHIELD_X, GfxConstants.TURN_SHIELD_Y));
 
-  public void tileFocusChangedAt(Position position) {
-    System.out.println( "Fake it: tileFocusChangedAt "+position );
-  }
+        // defineUnitMap();
+        setGameAge(age);
+    }
+
+    public void tileFocusChangedAt(Position position) {
+        System.out.println("TEST");
+    }
+
+
+
+
+    /* Added help methods */
+
+    private ImageFigure getBlackFigureAt(Point position) {
+        return new ImageFigure(GfxConstants.NOTHING, position);
+    }
+
+    private void setGameAge(int age) {
+        if (age < 0) {
+            gameAge.setText((age * (-1)) + "BC");
+        } else if (age == 0) {
+            gameAge.setText("0");
+        } else if (age > 0) {
+            gameAge.setText(age + "AD");
+        }
+    }
 }
